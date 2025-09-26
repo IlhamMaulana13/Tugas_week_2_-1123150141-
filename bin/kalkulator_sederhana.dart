9331aa67ebb9e844ded2c84ba6c42a47b4911b06
Ilhmmlna <mhmmad13ilhammlna@gmail.com>
2025-09-26 23:25:57 +0700

    second commit kalkulator (setting 0 pada pembagian)

--- bin/kalkulator_sederhana.dart
+++ bin/kalkulator_sederhana.dart
@@ -39,9 +39,12 @@ case '3':
   break;
 
 case '4':
-  print('Hasil: ${a / b}');
-  break;
-
+  if (b == 0) {
+          print('Error: Tidak bisa dibagi 0!');
+        } else {
+          print('Hasil: ${a / b}');
+        }
+        break;
 default:
 print('Pilihan tidak dikenal.');
 }
